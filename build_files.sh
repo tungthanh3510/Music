04f65430626e7ce9d6c055fe1a24e306ed4b6f47
TnGiaBaoGT <115094484+TnGiaBaoGT@users.noreply.github.com>
2024-05-21 16:50:50 +0700

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-# Create a virtual environment
+# Use the correct Python version
 python3 -m venv .venv
 
 # Activate the virtual environment
 source .venv/bin/activate
 
+# Upgrade pip
+pip install --upgrade pip
+
 # Install dependencies
 pip install -r requirements.txt
+
+# Run database migrations
+python manage.py migrate
 
 # Collect static files
 python manage.py collectstatic --noinput
