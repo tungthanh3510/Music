3d87a21d7136bad3c7828c7aa9a69ff0d4c002d5
TnGiaBaoGT <115094484+TnGiaBaoGT@users.noreply.github.com>
2024-05-21 16:42:37 +0700

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,19 @@
-python -m venv .venv
-source venv/Scripts/activate
-pip install -r requirements.txt 
+#!/bin/bash
+
+# Create a virtual environment
+python3 -m venv .venv
+
+# Activate the virtual environment
+source .venv/bin/activate
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Collect static files
 python manage.py collectstatic --noinput
+
+
+# python -m venv .venv
+# source venv/Scripts/activate
+# pip install -r requirements.txt 
+# python manage.py collectstatic --noinput
